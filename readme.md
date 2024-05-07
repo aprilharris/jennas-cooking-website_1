3fdaaf83863ca26921d5efa986ac70f5441a346d
aprilharris <54374224+aprilharris@users.noreply.github.com>
2024-05-07 17:30:55 -0400

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Jenna's Cooking Website
+
+## Jenna is an extremely passionate home cook, she loves creating her own recipes and enjoys showcasing her art online. She admits though, that it is hard for her to be inspired and create dishes out of thin air. She has reached out to your group to create a website for her that 1) Showcases her favorite dishes and 2) Has a page that can generate a random dish with ingredients she has.
+
+Your API: https://spoonacular.com/food-api
+
+Recipe Demo : https://jennas-recs.abraraot.repl.co
+
+# Color Palette
+
+   <!-- Coolors Palette Widget -->
+<!--       <script src="https://coolors.co/palette-widget/widget.js"></script>
+      <script data-id="04444577810981323">new CoolorsPaletteWidget("04444577810981323", ["5bc0eb","fde74c","9bc53d","e55934","fa7921"]); </script> -->
---
+++ script.js
@@ -0,0 +1,56 @@
+// apiKey
+// 7db1baf9ccbb44a78234fabcb60e7f5f 
+let url = "https://api.spoonacular.com/recipes/random?apiKey=7db1baf9ccbb44a78234fabcb60e7f5f&tags=vegan";
+
+function fetchRecipes(){
+  let ing1 = $(".ing1").val();
+  let ing2 = $(".ing2").val();
+  let ing3 = $(".ing3").val();
+  let temp = Math.floor((Math.random()*3)+1); // Used for showcase - Not needed for original. 
+  
+  let url2 = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=7db1baf9ccbb44a78234fabcb60e7f5f&ingredients="+ing1+","+ing2+","+ing3+"&number="+temp;
+  console.log(url2);
+
+  let request = new XMLHttpRequest();
+
+  request.open("GET", url2, true);
+  
+  request.onload = function() { //Once we recieve the response do this vvvv
+  
+    let data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+      
+      // console.log(data); - Educational Purposes 
+      recipe_img = data[0].image;
+      resTitle = data[0].title;
+      resId = data[0].id;
+      console.log(resId);
+      console.log(resTitle);
+      $(".result").html(resTitle)
+      $('.recipe-image').attr('src',recipe_img);
+      
+      //OPTIONAL EXTENTIAL - BUT SEEMS USEFUL
+      let urlN = "https://api.spoonacular.com/recipes/"+resId+"/information?apiKey=7db1baf9ccbb44a78234fabcb60e7f5f";
+      let newRequest = new XMLHttpRequest();
+      newRequest.open("GET", urlN, true);
+      newRequest.onload = function() { //Once we recieve the response do this vvvv
+
+        let newData = JSON.parse(this.response);
+        if (newRequest.status >= 200 && newRequest.status < 400) {
+          let resIns = newData.instructions;
+          console.log(resIns);
+
+          if(resIns == null){
+            $(".instro").text("No instructions are available at this time");
+          }
+          else{
+            $(".instro").html(resIns);
+          }  	      
+	      }
+      };
+      newRequest.send();
+    	//$("#advice").text(data.recipes[0].instructions);
+  	}
+  };
+  request.send();
+}
